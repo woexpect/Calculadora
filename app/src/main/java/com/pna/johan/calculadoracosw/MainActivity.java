add397a8e84531ba65a963b2501ce54dbd89d91e
woexpect <sebaspp95@gmail.com>
2017-03-08 09:59:19 -0500

    Calculator V2.0

---
+++ app/src/main/java/com/pna/johan/calculadoracosw/MainActivity.java
@@ -0,0 +1,71 @@
+package com.pna.johan.calculadoracosw;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Typeface latoFont = Typeface.createFromAsset(this.getAssets(), "Lato.ttf");
+    //Object.setTypeface(latoFont);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Hide Action Bar
+        getSupportActionBar().hide();
+    }
+
+    public void addSeven(View view) {
+    }
+
+    public void addEigth(View view) {
+    }
+
+    public void addNine(View view) {
+    }
+
+    public void addfour(View view) {
+    }
+
+    public void addFive(View view) {
+    }
+
+    public void addSix(View view) {
+    }
+
+    public void addOne(View view) {
+    }
+
+    public void addTwo(View view) {
+    }
+
+    public void addThree(View view) {
+    }
+
+    public void addPoint(View view) {
+    }
+
+    public void addZero(View view) {
+    }
+
+    public void solveOperation(View view) {
+    }
+
+    public void deleteAll(View view) {
+    }
+
+    public void operationDiv(View view) {
+    }
+
+    public void operationMult(View view) {
+    }
+
+    public void operationAdd(View view) {
+    }
+
+    public void operationSubstract(View view) {
+    }
+}
